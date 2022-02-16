938406732a28b7c2a691f5e6a8cba512be349ce5
Anych2 <anuar123@mail.ru>
2022-02-16 10:46:40 +0600

    added pagination in booking function

--- tickets/code/web/booking.py
+++ tickets/code/web/booking.py
@@ -5,86 +5,103 @@ from sanic import response
 
 async def booking_details(request, booking_id):
     async with request.app.ctx.db_pool.acquire() as conn:
-        details = await conn.fetch(f"SELECT * FROM booking WHERE booking_id = '{booking_id}'")
-        details = [{'id': d['id'], 'booking_id': d['booking_id'], 'pnr': d['pnr'], 'expires_at': d['expires_at'],
-                    'phone': d['phone'], 'email': d['email']
-                    } for d in details][0]
+        if request.args:
+            if 'email' in request.args:
+                all_bookings = await conn.fetch(f"SELECT * FROM booking "
+                                                f"WHERE email = '{request.args['email']}' "
+                                                f"AND phone = '{request.args['phone']}' ORDER BY id")
+                return response.json(all_bookings, dumps=json.dumps, default=str)
+            else:
 
-        offer_details = await conn.fetch(f"SELECT * FROM offer WHERE booking_id = '{details['id']}'")
-        offer_details = [{'id': d['id'], 'offer_id': d['offer_id'], 'flights': None, 'refundable': d['refundable'],
-                          'baggage': d['baggage'], 'cabin': d['cabin'], 'airline': d['airline'],
-                          'passengers': d['passengers'], 'type': d['type']} for d in offer_details][0]
+                pagination = await conn.fetch(f"SELECT * FROM booking "
+                                              f"WHERE email = '{request.args['email']}' "
+                                              f"AND phone = '{request.args['phone']}' ORDER BY id"
+                                              f"LIMIT {request.args['limit']} OFFSET {request.args['page']} * 10")
+                return response.json(pagination, dumps=json.dumps, default=str)
+        else:
+            details = await conn.fetch(f"SELECT * FROM booking WHERE booking_id = '{booking_id}'")
+            details = [{'id': d['id'], 'booking_id': d['booking_id'], 'pnr': d['pnr'], 'expires_at': d['expires_at'],
+                        'phone': d['phone'], 'email': d['email']
+                        } for d in details][0]
 
-        flights_details = await conn.fetch(f"SELECT * FROM flight WHERE offer_id = '{offer_details['id']}'")
-        flights_details = [{'id': d['id'], 'duration': d['duration']} for d in flights_details]
+            offer_details = await conn.fetch(f"SELECT * FROM offer WHERE booking_id = '{details['id']}'")
+            offer_details = [{'id': d['id'], 'offer_id': d['offer_id'], 'flights': None, 'price': None,
+                              'refundable': d['refundable'], 'baggage': d['baggage'], 'cabin': d['cabin'],
+                              'airline': d['airline'], 'passengers': d['passengers'], 'type': d['type']
+                              } for d in offer_details][0]
 
-        segments = []
-        for num in range(len(flights_details)):
-            segment_details = await conn.fetch(f"SELECT * FROM segment WHERE flight_id = '{flights_details[num]['id']}'")
-            segment_details = [{'id': d['id'], 'operating_airline': d['operating_airline'],
-                                'flight_number': d['flight_number'], 'equipment': d['equipment'], 'cabin': d['cabin'],
-                                'dep': d['departure_id'], 'arr': d['arrive_id'], 'baggage': d['baggage'],
-                                } for d in segment_details]
+            flights_details = await conn.fetch(f"SELECT * FROM flight WHERE offer_id = '{offer_details['id']}'")
+            flights_details = [{'id': d['id'], 'duration': d['duration']} for d in flights_details]
 
-            departure_route = await conn.fetch(
-                f"SELECT * FROM route_point WHERE id = '{segment_details[0]['dep']}'")
-            departure_route = [{'at': d['time_at'], 'airport': d['airport_id'],
-                                'terminal': d['terminal']} for d in departure_route]
-            segment_details[0]['dep'] = departure_route
-            departure_airport = await conn.fetch(f"SELECT * FROM airport "
-                                                 f"WHERE id = '{departure_route[0]['airport']}'")
-            departure_airport = [{'code': d['code'], 'name': d['name']} for d in departure_airport]
-            departure_route[0]['airport'] = departure_airport
-            segment_details[0]['dep'] = departure_route
+            segments = []
+            for num in range(len(flights_details)):
+                segment_details = await conn.fetch(f"SELECT * FROM segment "
+                                                   f"WHERE flight_id = '{flights_details[num]['id']}'")
+                segment_details = [{'id': d['id'], 'operating_airline': d['operating_airline'],
+                                    'flight_number': d['flight_number'], 'equipment': d['equipment'],
+                                    'cabin': d['cabin'], 'dep': d['departure_id'], 'arr': d['arrive_id'],
+                                    'baggage': d['baggage']} for d in segment_details]
 
-            arrive_route = await conn.fetch(f"SELECT * FROM route_point "
-                                            f"WHERE airport_id = '{segment_details[0]['arr']}'")
-            arrive_route = [{'time_at': d['time_at'], 'airport': d['airport_id'],
-                             'terminal': d['terminal']} for d in arrive_route]
-            arrive_airport = await conn.fetch(f"SELECT * FROM airport WHERE id = '{arrive_route[0]['airport']}'")
-            arrive_airport = [{'code': d['code'], 'name': d['name']} for d in arrive_airport]
-            arrive_route[0]['airport'] = arrive_airport
-            segment_details[0]['arr'] = arrive_route
-            del segment_details[0]['id']
-            segments.append(segment_details[0])
+                departure_route = await conn.fetch(
+                    f"SELECT * FROM route_point WHERE id = '{segment_details[0]['dep']}'")
+                departure_route = [{'at': d['time_at'], 'airport': d['airport_id'],
+                                    'terminal': d['terminal']} for d in departure_route]
+                segment_details[0]['dep'] = departure_route
+                departure_airport = await conn.fetch(f"SELECT * FROM airport "
+                                                     f"WHERE id = '{departure_route[0]['airport']}'")
+                departure_airport = [{'code': d['code'], 'name': d['name']} for d in departure_airport]
+                departure_route[0]['airport'] = departure_airport
+                segment_details[0]['dep'] = departure_route
 
-        price = await conn.fetch(f"SELECT * FROM price WHERE offer_id = '{offer_details['id']}'")
-        price = [{'price': d['price'], 'currency': d['currency']} for d in price]
+                arrive_route = await conn.fetch(f"SELECT * FROM route_point "
+                                                f"WHERE airport_id = '{segment_details[0]['arr']}'")
+                arrive_route = [{'time_at': d['time_at'], 'airport': d['airport_id'],
+                                 'terminal': d['terminal']} for d in arrive_route]
+                arrive_airport = await conn.fetch(f"SELECT * FROM airport WHERE id = '{arrive_route[0]['airport']}'")
+                arrive_airport = [{'code': d['code'], 'name': d['name']} for d in arrive_airport]
+                arrive_route[0]['airport'] = arrive_airport
+                segment_details[0]['arr'] = arrive_route
+                del segment_details[0]['id']
+                segments.append(segment_details[0])
 
-        airline = await conn.fetch(f"SELECT * FROM airline WHERE id = '{offer_details['airline']}'")
-        airline = [{'code': d['code'], 'name': d['name'], 'logo': d['logo_id']} for d in airline]
+            price = await conn.fetch(f"SELECT * FROM price WHERE offer_id = '{offer_details['id']}'")
+            price = [{'price': int(d['price']), 'currency': d['currency']} for d in price]
 
-        logos = []
-        for num in range(len(airline)):
-            logo = await conn.fetch(f"SELECT * FROM logo WHERE id = '{airline[num]['logo']}'")
-            logo = [{'url': d['url'], 'height': d['height'], 'weight': d['weight']} for d in logo]
-            airline[num]['logo'] = logo[0]
-            logos.append(airline[num])
+            airline = await conn.fetch(f"SELECT * FROM airline WHERE id = '{offer_details['airline']}'")
+            airline = [{'code': d['code'], 'name': d['name'], 'logo': d['logo_id']} for d in airline]
 
-        passengers_qty = await conn.fetch(f"SELECT * FROM passengers WHERE id = '{offer_details['passengers']}'")
-        passengers_qty = [{'ADT': d['ADT'], 'CHD': d['CHD'], 'INF': d['INF']} for d in passengers_qty]
+            logos = []
+            for num in range(len(airline)):
+                logo = await conn.fetch(f"SELECT * FROM logo WHERE id = '{airline[num]['logo']}'")
+                logo = [{'url': d['url'], 'height': d['height'], 'weight': d['weight']} for d in logo]
+                airline[num]['logo'] = logo[0]
+                logos.append(airline[num])
 
-        passenger = await conn.fetch(f"SELECT * FROM passenger WHERE booking_id = '{details['id']}'")
-        passenger = [{'gender': d['gender'], 'type': d['type'], 'first_name': d['first_name'],
-                      'last_name': d['last_name'], 'date_of_birth': d['date_of_birth'], 'citizenship': d['citizenship'],
-                      'document': d['document_id']} for d in passenger]
+            passengers_qty = await conn.fetch(f"SELECT * FROM passengers WHERE id = '{offer_details['passengers']}'")
+            passengers_qty = [{'ADT': d['ADT'], 'CHD': d['CHD'], 'INF': d['INF']} for d in passengers_qty]
 
-        passengers = []
-        for num in range(len(passenger)):
-            document = await conn.fetch(f"SELECT * FROM document WHERE id = '{passenger[num]['document']}'")
-            document = [{'number': d['number'], 'expires_at': d['expires_at'], 'iin': d['iin']} for d in document]
-            passenger[num]['document'] = document[0]
-            passengers.append(passenger[num])
+            passenger = await conn.fetch(f"SELECT * FROM passenger WHERE booking_id = '{details['id']}'")
+            passenger = [{'gender': d['gender'], 'type': d['type'], 'first_name': d['first_name'],
+                          'last_name': d['last_name'], 'date_of_birth': d['date_of_birth'],
+                          'citizenship': d['citizenship'], 'document': d['document_id']} for d in passenger]
 
-        del details['id']
-        del offer_details['id']
-        for num in range(len(flights_details)):
-            del flights_details[num]['id']
-            flights_details[num]['segments'] = segments[num]
+            passengers = []
+            for num in range(len(passenger)):
+                document = await conn.fetch(f"SELECT * FROM document WHERE id = '{passenger[num]['document']}'")
+                document = [{'number': d['number'], 'expires_at': d['expires_at'], 'iin': d['iin']} for d in document]
+                passenger[num]['document'] = document[0]
+                passengers.append(passenger[num])
 
-        details['offer'] = [offer_details]
-        details['offer'][0]['flights'] = [flights_details]
-        details['offer'][0]['airline'] = airline
-        details['offer'][0]['passengers'] = passengers_qty
-        details['passengers'] = passengers
-    return response.json(details, dumps=json.dumps, default=str)
+            del details['id']
+            del offer_details['id']
+            for num in range(len(flights_details)):
+                del flights_details[num]['id']
+                flights_details[num]['segments'] = segments[num]
+
+            details['offer'] = [offer_details]
+            details['offer'][0]['flights'] = [flights_details]
+            details['offer'][0]['price'] = price[0]
+            details['offer'][0]['airline'] = airline
+            details['offer'][0]['passengers'] = passengers_qty
+            details['passengers'] = passengers
+        return response.json(details, dumps=json.dumps, default=str)
