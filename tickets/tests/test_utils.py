0c29091c40a7c65465f7b69865aa372d7fe02ddb
Anych2 <anuar123@mail.ru>
2022-02-28 17:42:38 +0600

    Started to connect db

--- tickets/tests/test_utils.py
+++ tickets/tests/test_utils.py
@@ -4,12 +4,12 @@ from aioredis import Redis
 
 from code.utils import get_currency_rates
 
-#
-# async def test_currency_rates(app, mocker):
-#     currency_dict = {'AUD': 400}
-#     redis = MagicMock()
-#     hgetall_resp = AsyncMock(return_value=currency_dict)
-#     mocker.patch.object(Redis, 'hgetall', side_effect=hgetall_resp)
-#     currencies = await get_currency_rates(redis)
-#
-#     assert currencies == currency_dict
+
+async def test_currency_rates(app, mocker):
+    currency_dict = {'AUD': 400}
+    redis = MagicMock()
+    hgetall_resp = AsyncMock(return_value=currency_dict)
+    mocker.patch.object(Redis, 'hgetall', side_effect=hgetall_resp)
+    currencies = await get_currency_rates(redis)
+
+    assert currencies == currency_dict
