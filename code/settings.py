6bddace0b488304458ae0ed93407cfb85b3788e6
Anych2 <anuar123@mail.ru>
2022-03-09 04:36:57 +0600

    MR for deploying

--- code/settings.py
+++ code/settings.py
@@ -4,4 +4,4 @@ DATABASE_URL = os.environ['DATABASE_URL']
 DEBUG = os.environ['DEBUG']
 REDIS_URL = os.environ['REDIS_URL']
 SEARCH_EXPIRE_TIME = 20 * 60
-TEST_DATABASE_URL = 'postgres://postgres:123@postgres:5432/postgres?sslmode=disable'
+
